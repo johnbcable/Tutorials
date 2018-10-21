d5b18bdf2c33116651a3961f318051a0dd75b811
John Cable <johnbcable@me.com>
2018-10-21 21:54:22 +0100

    Tutorial 2 in the Three.js series

--- Graphics/ThreeJS/tutorial02/scripts/main.js
+++ Graphics/ThreeJS/tutorial02/scripts/main.js
@@ -26,7 +26,7 @@ document.body.appendChild( renderer.domElement );
 var geometry = new THREE.TorusKnotBufferGeometry( 4, .5, 60, 60, 13, 10 );
 
 // create a material 
-var material = new THREE.MeshNormalMaterial();
+var material = new THREE.MeshStandardMaterial();
 /*
 var material = new THREE.MeshNormalMaterial({
 	wireframe:true
@@ -35,39 +35,72 @@ var material = new THREE.MeshNormalMaterial({
 
 // create a mesh from the geometry and material
 var myobject = new THREE.Mesh( geometry, material );
+myobject.castShadow = true;
 
 // add the cube to the scene
 scene.add( myobject );
 
+/* create an ambient light
+var light = new THREE.AmbientLight(0xffeec, .4);
+scene.add( light );
+light.position.set(3, 4, 15);
+*/
+
+// create a primary spotlight
+var light = new THREE.SpotLight(0xfffff, .6);
+light.castShadow = true;
+light.shadow.mapSize.width = 2048;
+light.shadow.mapSize.height = 2048;
+light.shadow.camera.near = 0.1;
+light.shadow.camera.far = 500;
+scene.add( light );
+light.position.set(3, 4, 15);
+
+// create a secondary spotlight
+var seclight = new THREE.SpotLight(0xf0000, .4);
+seclight.castShadow = true;
+seclight.shadow.mapSize.width = 2048;
+seclight.shadow.mapSize.height = 2048;
+seclight.shadow.camera.near = 0.1;
+seclight.shadow.camera.far = 500;
+scene.add( seclight );
+seclight.position.set(-5, 15, 5);
+
+// Create a ground plane
+var planeGeometry = new THREE.PlaneBufferGeometry( 500, 500, 32, 32 );
+var planeMaterial = new THREE.MeshStandardMaterial( {
+	color: 0xeeaa33
+});
+var plane = new THREE.Mesh( planeGeometry, planeMaterial );
+plane.rotation.x = Math.PI/180*-90;
+plane.position.y = -6;
+plane.receiveShadow = true;
+scene.add( plane );
+
+// Create a back wall
+var planeGeometry2 = new THREE.PlaneBufferGeometry( 500, 500, 32, 32 );
+var planeMaterial2 = new THREE.MeshStandardMaterial( {
+	color: 0xeeaa33
+});
+var plane2 = new THREE.Mesh( planeGeometry, planeMaterial );
+plane2.position.z = -50;
+plane2.receiveShadow = true;
+scene.add( plane2 );
+
+
 // render the scene
-var render = function () {
-	requestAnimationFrame( render );
+var animate = function () {
+	requestAnimationFrame( animate );
+	myobject.rotation.x += 0.01;
+	myobject.rotation.y += 0.03;
+	// Update renderer to use shadow map
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-	/* set position
-	cube.position.x=1;
-	cube.position.z=.1;
-	cube.position.y=-.1;
+	renderer.render( scene, camera );
+}
+animate();
 
-	// rotate the cube
-	cube.rotation.y=Math.PI/180 * 45;
-	cube.rotation.z=Math.PI/180 * -25;
-	*/
-
-	cube.rotation.x += 0.01;
-	cube.rotation.z -= 0.01;
-	cube.rotation.y -= 0.02;
-
-	/*
-	cube.position.x = 2 *Math.sin(cube.rotation.x);
-	cube.position.z = 2 *Math.sin(cube.rotation.z);
-	*/
-	
-	cube.position.x = .5 * Math.sin(cube.rotation.z);
-	cube.position.z = 3 * Math.sin(cube.rotation.x + cube.rotation.y);
-
-	renderer.render( scene, camera );	
-};
-render();
 
 // window resize handler
 window.addEventListener( 'resize', function () {
