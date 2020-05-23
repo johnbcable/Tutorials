70b05be0a60ecdce814de0b22959af19ecd9457e
John Cable <johncable@talk21.com>
2020-05-23 13:20:59 +0100

    Update from MacBook

---
+++ GraphQL/customerbase/schema.js
@@ -0,0 +1,16 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = require('graphql');
+
+
+
+
+
+module.exports = new GraphQLSchema({
+
+});
